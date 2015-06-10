9ce16cd22875bed04ba3467c999c709ede9a7203
houhai.duan <houhai.duan@happyelements.com>
2015-06-10 11:26:16 +0800

    new function

---
+++ Cameras/AbstractCamera.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cameras
+{
+    public abstract class AbstractCamera : MonoBehaviour    {
+        
+        public enum UpdateType // The available methods of updating are:
+        {
+            Update,//normal update
+            FixedUpdate, // Update in FixedUpdate (for tracking rigidbodies).
+            LateUpdate, // Update in LateUpdate. (for tracking objects that are moved in Update)
+            ManualUpdate, // user must call to update camera
+        }
+
+        [SerializeField] private UpdateType m_UpdateType;
+
+
+        void Update()
+        {
+            if (m_UpdateType == UpdateType.Update)
+            {
+                parseInput(Time.deltaTime);
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (m_UpdateType == UpdateType.FixedUpdate)
+            {
+                parseInput(Time.deltaTime);
+            }
+        }
+
+
+        void LateUpdate()
+        {
+            if (m_UpdateType == UpdateType.LateUpdate)
+            {
+                parseInput(Time.deltaTime);
+            }
+        }
+
+
+        void ManualUpdate()
+        {
+            if (m_UpdateType == UpdateType.ManualUpdate)
+            {
+                parseInput(Time.deltaTime);
+            }
+        }
+
+        protected abstract void parseInput(float deltaTime);
+    }
+}
---
+++ Cameras/FreeCamera.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Cameras
+{
+    public class FreeCamera : AbstractCamera
+    {
+        [SerializeField] private float m_MoveSpeed = 10f;
+        [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;
+        [SerializeField] private bool m_LockCursor = false; 
+
+        // Update is called once per frame
+        protected override void parseInput(float deltaTime)
+        {
+            //处理转动
+            parseTurn(deltaTime);
+            
+            //处理移动
+            parseMove(deltaTime);
+
+            if (m_LockCursor)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Cursor.visible = false;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    Cursor.visible = true;
+                }
+                
+            }
+        }
+
+        protected void parseTurn(float deltaTime)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Transform transform = GetComponent<Transform>();
+
+                float mouseX = CrossPlatformInputManager.GetAxis("Mouse X");
+                float mouseY = CrossPlatformInputManager.GetAxis("Mouse Y");
+
+                //Vector3 eulerAngles=transform.rotation.eulerAngles;
+                float angleY = mouseX * m_TurnSpeed;
+                float angleX = mouseY * -m_TurnSpeed;
+
+                transform.Rotate(angleX, angleY, 0);
+            }
+        }
+
+        protected void parseMove(float deltaTime) 
+        {
+            bool haveTranslate = false;
+
+            Vector3 translate = new Vector3(0f, 0f, 0f);
+
+            if (Input.GetKey("w"))
+            {
+                translate.z += deltaTime * m_MoveSpeed;
+                haveTranslate = true;
+            }
+
+            if (Input.GetKey("s"))
+            {
+                translate.z -= deltaTime * m_MoveSpeed;
+                haveTranslate = true;
+            }
+
+            if (Input.GetKey("a"))
+            {
+                translate.x -= deltaTime * m_MoveSpeed;
+                haveTranslate = true;
+            }
+
+            if (Input.GetKey("d"))
+            {
+                translate.x += deltaTime * m_MoveSpeed;
+                haveTranslate = true;
+            }
+
+            if (haveTranslate)
+            {
+                Transform transform = GetComponent<Transform>();
+
+                transform.Translate(translate);
+            }
+        }
+    }
+}
