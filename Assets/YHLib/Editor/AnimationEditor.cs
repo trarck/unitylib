e284b22542b02bb7eddc00ba5ad625efffde6fe7
trarck <trarck@gmail.com>
2015-08-14 08:36:33 +0800

    new panel

---
+++ Assets/YHLib/Editor/AnimationEditor.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AnimationEditor : Editor
+{
+	[MenuItem ("Animation/Fix Path")]
+	public static void FixPath(){
+
+		AnimationClip ac = Selection.activeObject as AnimationClip;
+
+		GameObject panel = GameObject.Find("panel");
+	
+
+		if (ac is AnimationClip) {
+
+			Debug.Log("****************---->");
+			EditorCurveBinding[] bindings=AnimationUtility.GetCurveBindings (ac);
+			for (int i=0;i<bindings.Length;++i)
+			{
+				EditorCurveBinding binding=bindings[i];
+				Debug.Log("binding path="+binding.path+",propName="+binding.propertyName+",PPtrCurve="+binding.isPPtrCurve+",type="+binding.type);
+
+				AnimationCurve aniCurve=AnimationUtility.GetEditorCurve(ac,binding);
+	
+				Object t=AnimationUtility.GetAnimatedObject(panel,binding);
+
+				if(t){
+					Debug.Log("test:"+t);
+				}else{
+					Debug.Log("can't find:"+binding.path);
+					string path=binding.path;
+
+					int pos=path.IndexOf("/");
+
+					path=path.Substring(pos+1);
+
+					Debug.Log("lookFor:"+path);
+
+					t=panel.transform.Find(path).gameObject;
+
+					string newPath=AnimationUtility.CalculateTransformPath((t as GameObject).transform,panel.transform);
+					Debug.Log("path:"+newPath);
+
+					AnimationUtility.SetEditorCurve(ac,binding,null);
+					binding.path=newPath;
+					AnimationUtility.SetEditorCurve(ac,binding,aniCurve);
+				}
+			}
+
+			AssetDatabase.SaveAssets();
+
+			Debug.Log("****************<----");
+
+		}else{
+			Debug.Log("not AnimationClip");
+			Debug.Log((Selection.activeObject));
+		}
+		
+//		AnimationUtility.onCurveWasModified = OnCurveWasModified;
+		/*
+		AnimationClipCurveData[] datas = AnimationUtility.GetAllCurves ();
+		for (int i=0; i<datas.Length; ++i) {
+			Debug.Log(datas[i].ToString());
+		}
+		*/
+	}
+
+	public static void OnCurveWasModified(AnimationClip clip, EditorCurveBinding binding, AnimationUtility.CurveModifiedType deleted)
+	{
+		Debug.Log("onEvent:"+binding.path+","+deleted);
+		binding.path = "Cube";
+	}
+}
---
+++ Assets/YHLib/Editor/ClipInfo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// Editor window for listing all object reference curves in an animation clip
+public class ClipInfo : EditorWindow
+{
+    private AnimationClip clip;
+
+    [MenuItem("MyMenu/Clip Info")]
+    static void Init()
+    {
+        GetWindow(typeof(ClipInfo));
+    }
+
+    public void OnGUI()
+    {
+        clip = EditorGUILayout.ObjectField("Clip", clip, typeof(AnimationClip), false) as AnimationClip;
+
+        EditorGUILayout.LabelField("Curves:");
+        if (clip != null)
+        {
+            foreach (var binding in AnimationUtility.GetCurveBindings(clip))
+            {
+                AnimationCurve curve = AnimationUtility.GetEditorCurve(clip, binding);
+                EditorGUILayout.LabelField(binding.path + "/" + binding.propertyName + ", Keys: " + curve.keys.Length);
+            }
+        }
+    }
+}
