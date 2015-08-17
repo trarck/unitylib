7d7a478c6f437578ff5bc6d08bbd9301c569e130
trarck <trarck@gmail.com>
2015-08-17 08:59:12 +0800

    add fix window

--- Assets/YHLib/Editor/AnimationEditor.cs
+++ Assets/YHLib/Editor/AnimationEditor.cs
@@ -2,73 +2,136 @@
 using System.Collections;
 using UnityEditor;
 
-public class AnimationEditor : Editor
+public class AnimationEditor : EditorWindow
 {
+
+    string m_Path = "";
+    AnimationClip clip;
+
 	[MenuItem ("Animation/Fix Path")]
-	public static void FixPath(){
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(AnimationEditor));
+    }
 
-		AnimationClip ac = Selection.activeObject as AnimationClip;
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
 
-		GameObject panel = GameObject.Find("panel");
-	
+        clip = EditorGUILayout.ObjectField("Clip", clip, typeof(AnimationClip), false) as AnimationClip;
 
-		if (ac is AnimationClip) {
+        m_Path = EditorGUILayout.TextField("path", m_Path);
 
-			Debug.Log("****************---->");
-			EditorCurveBinding[] bindings=AnimationUtility.GetCurveBindings (ac);
-			for (int i=0;i<bindings.Length;++i)
-			{
-				EditorCurveBinding binding=bindings[i];
-				Debug.Log("binding path="+binding.path+",propName="+binding.propertyName+",PPtrCurve="+binding.isPPtrCurve+",type="+binding.type);
+        if (GUILayout.Button("fix"))
+        {
+            AutoFix();
+        }
+    }
 
-				AnimationCurve aniCurve=AnimationUtility.GetEditorCurve(ac,binding);
-	
-				Object t=AnimationUtility.GetAnimatedObject(panel,binding);
+    void AutoFix()
+    {
+        if (clip)
+        {
+            GameObject root = Selection.activeGameObject;
+            if (root)
+            {
+                EditorCurveBinding[] bindings = AnimationUtility.GetCurveBindings(clip);
+                for (int i = 0; i < bindings.Length; ++i)
+                {
+                    EditorCurveBinding binding = bindings[i];
+                    Debug.Log("binding path=" + binding.path + ",propName=" + binding.propertyName + ",PPtrCurve=" + binding.isPPtrCurve + ",type=" + binding.type);
 
-				if(t){
-					Debug.Log("test:"+t);
-				}else{
-					Debug.Log("can't find:"+binding.path);
-					string path=binding.path;
+                    
 
-					int pos=path.IndexOf("/");
+                    GameObject animatedObject = AnimationUtility.GetAnimatedObject(root, binding) as GameObject;
 
-					path=path.Substring(pos+1);
+                    if (!animatedObject)
+                    {
+                        //不存在则修正
+                        //Debug.Log("can't find:" + binding.path);
+                        //二种修正，一种是上移，从子节点移到父结点。一种是下移，从父节点移到子结点。
 
-					Debug.Log("lookFor:"+path);
+                        string path = binding.path;
 
-					t=panel.transform.Find(path).gameObject;
+                        //查找存在的父结点。存在的就是没有被移动的。
+                        string[] result = LookForExists(root, path);
+                        //自动修正，只能修正不会有重名的元素。
+                        //比如根结点有个Name，Panel有个Name,把Name移到，PanelTT下，则可能无法正确找到PanelTT下的Name
 
-					string newPath=AnimationUtility.CalculateTransformPath((t as GameObject).transform,panel.transform);
-					Debug.Log("path:"+newPath);
+                        //获取被移动GameObject
+                        animatedObject = FindUtil.SearchGameObject(result[1], root);
+                        if (!animatedObject)
+                        {
+                            Debug.LogWarning("can't find:" + result[1]);
+                            continue;
+                        }
 
-					AnimationUtility.SetEditorCurve(ac,binding,null);
-					binding.path=newPath;
-					AnimationUtility.SetEditorCurve(ac,binding,aniCurve);
-				}
-			}
+                        string newPath = AnimationUtility.CalculateTransformPath(animatedObject.transform, root .transform);
+                        Debug.Log("fix path:" + path+",to:"+newPath);
 
-			AssetDatabase.SaveAssets();
+                        AnimationCurve aniCurve = AnimationUtility.GetEditorCurve(clip, binding);
+                        //remove old
+                        AnimationUtility.SetEditorCurve(clip, binding, null);
+                        binding.path = newPath;
+                        //add new
+                        AnimationUtility.SetEditorCurve(clip, binding, aniCurve);
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogWarning("root is null");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("AnimationClip is null");
+        }
+    }
 
-			Debug.Log("****************<----");
+    string[] LookForExists(GameObject root,string path)
+    {
+        string[] result = new string[2];
 
-		}else{
-			Debug.Log("not AnimationClip");
-			Debug.Log((Selection.activeObject));
-		}
-		
-//		AnimationUtility.onCurveWasModified = OnCurveWasModified;
-		/*
-		AnimationClipCurveData[] datas = AnimationUtility.GetAllCurves ();
-		for (int i=0; i<datas.Length; ++i) {
-			Debug.Log(datas[i].ToString());
-		}
-		*/
-	}
+        string[] paths = path.Split('/');
 
-	public static void OnCurveWasModified(AnimationClip clip, EditorCurveBinding binding, AnimationUtility.CurveModifiedType deleted)
-	{
-		Debug.Log("onEvent:"+binding.path+","+deleted);
-		binding.path = "Cube";
+        if (paths.Length == 1)
+        {
+            result[0] = "";
+            result[1] = path;
+        }
+        else
+        {
+            string tempPath = "";
+            int i = 0;
+            for (; i < paths.Length; ++i)
+            {
+                tempPath += (i>0?"/":"") + paths[i];
+
+                if (root.transform.Find(tempPath) == null)
+                {
+                    break;
+                }
+                else
+                {
+                    result[0] = tempPath;
+                }
+            }
+
+            for (; i < paths.Length; ++i)
+            {
+                result[1] += "/"+paths[i];
+            }
+
+            //remove first "/"
+            result[1] = result[1].Substring(1);
+        }
+
+        return result;
+    }
+
+	void FixPath(){
+
 	}
 }
