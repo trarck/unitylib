f5083e7627d6541d6d13ab14ebcb1192cc33f713
houhai.duan <houhai.duan@happyelements.com>
2015-08-28 18:35:53 +0800

    temp

---
+++ Assets/YHLib/Scripts/2D/Parallax/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Parallax : MonoBehaviour
+{
+    List<ParallaxNode> m_Nodes=new List<ParallaxNode>();
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
---
+++ Assets/YHLib/Scripts/2D/Parallax/ParallaxNode.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxNode : MonoBehaviour 
+{
+    [SerializeField]
+    Vector2 m_Ratio=Vector2.zero;
+
+    [SerializeField]
+    Vector2 m_Offset=Vector2.zero;
+
+    Transform m_Transform=null;
+
+    void Start()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
+
+    public void setPosition(Vector2 pos)
+    {
+        m_Transform.localPosition = pos;
+    }
+
+    //==========================generate auto==============================//
+    public Vector2 ratio
+    {
+        set
+        {
+            m_Ratio = value;
+        }
+
+        get
+        {
+            return m_Ratio;
+        }
+    }
+
+    public Vector2 offset
+    {
+        set
+        {
+            m_Offset = value;
+        }
+
+        get
+        {
+            return m_Offset;
+        }
+    }
+}
