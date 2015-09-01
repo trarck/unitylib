cecb169ed53cce8fa760edf34b10ee149e448ab4
houhai.duan <houhai.duan@happyelements.com>
2015-09-01 18:16:35 +0800

    temp

---
+++ Assets/YHLib/Scripts/2D/Parallax/RepeatNode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RepeatNode : ParallaxNode {
+
+    [SerializeField]
+    float m_ElementSize = 8.0f;
+
+    Queue<Transform> m_Elements;
+
+	void Start () 
+    {
+        base.Start();
+        m_Elements = new Queue<Transform>();
+
+        InitElements();
+	}
+
+    void Update()
+    {
+
+    }
+
+    void InitElements()
+    {
+        for (int i = 0, l = m_Transform.childCount; i < l; ++i)
+        {
+            Transform child = m_Transform.GetChild(i);
+
+            AddElement(child);
+        }
+    }
+	
+	public void AddElement (Transform element) 
+    {
+        m_Elements.Enqueue(element);
+	}
+}
