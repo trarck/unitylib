87f2cbac11819ce80206bca9a495ec2205ab6d2d
trarck <trarck@gmail.com>
2015-09-03 08:05:16 +0800

    加入自动移动的功能。

--- Assets/Tests/Scripts/MoveTest.cs
+++ Assets/Tests/Scripts/MoveTest.cs
@@ -6,6 +6,9 @@ public class MoveTest : MonoBehaviour {
     [SerializeField]
     float m_Speed=1.0f;
 
+    [SerializeField]
+    bool m_AutoMove = false;
+
     Transform m_Transform;
 	// Use this for initialization
 	void Start () {
@@ -16,10 +19,11 @@ public class MoveTest : MonoBehaviour {
 	void Update () 
     {
 
-        if (Input.GetButton("Fire1"))
+        if (m_AutoMove || Input.GetButton("Fire1"))
         {
 
-            float mouseX = Input.GetAxis("Mouse X");
+            float mouseX = m_AutoMove?1.0f:Input.GetAxis("Mouse X");
+
             Vector3 pos = m_Transform.position;
 
             pos.x += mouseX*m_Speed * Time.deltaTime;
