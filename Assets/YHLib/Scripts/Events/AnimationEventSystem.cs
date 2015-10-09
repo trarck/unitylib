0c54377cad512deb6c74dff3ccda892ce54980db
houhai.duan <houhai.duan@happyelements.com>
2015-10-09 14:31:52 +0800

    add event

---
+++ Assets/YHLib/Scripts/Events/AnimationEventSystem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum AnimationEventType
+{
+    AnimationStart = 1,
+    AnimationComplete,
+
+
+}
+
+public class AnimationEventArgs : EventArgs 
+{
+
+	private int m_Message;
+
+    public AnimationEventArgs(int message)
+	{
+        m_Message = message;
+	}
+
+	public int message
+	{
+        get { return m_Message; }
+	}
+}
+
+public class AnimationEventSystem : MonoBehaviour {
+
+    public delegate void AnimationEventHandler(AnimationEventArgs e);
+
+    public event AnimationEventHandler OnAnimationEvent;
+
+    void ReceiveEvent(int type)
+    {
+        if (OnAnimationEvent!=null)
+        {
+            OnAnimationEvent(new AnimationEventArgs(type));
+        }
+        
+    }
+
+    public void TriggerCompleteEvent()
+    {
+        if (OnAnimationEvent != null)
+        {
+            OnAnimationEvent(new AnimationEventArgs((int)AnimationEventType.AnimationComplete));
+        }
+    }
+}
