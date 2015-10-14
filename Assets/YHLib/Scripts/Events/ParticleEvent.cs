99a8f36785407697b4210c5b26718661d3133fde
houhai.duan <houhai.duan@happyelements.com>
2015-10-14 10:57:34 +0800

    add ParticleEvent

---
+++ Assets/YHLib/Scripts/Events/ParticleEvent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace YH
+{
+    public class ParticleEvent : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            ParticleSystem ps = GetComponent<ParticleSystem>();
+            if (ps)
+            {
+                StartCoroutine(OnComplete(ps.duration));
+            }
+        }
+
+        IEnumerator OnComplete(float wait)
+        {
+            yield return new WaitForSeconds(wait);
+
+            AnimationEventSystem animationEventSystem = GetComponent<AnimationEventSystem>();
+            if (animationEventSystem != null)
+            {
+                animationEventSystem.TriggerCompleteEvent();
+            }
+        }
+    }
+}
