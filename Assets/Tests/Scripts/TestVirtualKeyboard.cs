74511fcc8db539183f36e319e31067429c7a5f11
trarck <trarck@gmail.com>
2015-12-21 15:19:32 +0800

    虚拟键盘。

---
+++ Assets/Tests/Scripts/TestVirtualKeyboard.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TestVirtualKeyboard : MonoBehaviour {
+    [SerializeField]
+    InputField m_InputField;
+
+    bool m_Focused;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(m_InputField)
+        {
+            if (m_Focused)
+            {
+                if (!m_InputField.isFocused)
+                {
+                    m_Focused = false;
+                    OnUnFocus();
+                }
+            }
+            else
+            {
+                if (m_InputField.isFocused)
+                {
+                    m_Focused = true;
+                    OnFocus();
+                }
+            }
+        }
+    }
+
+    void OnFocus()
+    {
+        Debug.Log("Focus");
+
+        Show();
+    }
+
+    void OnUnFocus()
+    {
+        Debug.Log("unFocus");
+        Hide();
+    }
+
+
+    public void Show()
+    {
+        VirtualKeyboard.ShowTouchKeyboard();
+    }
+
+    public void Hide()
+    {
+        VirtualKeyboard.HideTouchKeyboard();
+    }
+
+    void OnSelect(BaseEventData data)
+    {
+        Debug.Log(data);
+    }
+}
