a625f125be5a242c80fafc3c6129b4197e62747f
trarck <trarck@gmail.com>
2016-01-07 11:49:42 +0800

    加入截屏

---
+++ Assets/Tests/Scripts/TestScreenshot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using YH;
+
+public class TestScreenshot
+{
+    public string GetPhotoPath(string name)
+    {
+        return Application.persistentDataPath + "/" + name + ".png";
+    }
+
+    public string GetSavePhotoPath(string name)
+    {
+#if UNITY_EDITOR
+        return Application.persistentDataPath + "/" + name + ".png";
+#else  //UNITY_IOS || UNITY_ANDROID
+        return name + ".png";
+#endif
+    }
+
+    public void SaveScreenshot()
+    {
+        Rect captureRect = new Rect(0, 0, Camera.main.pixelWidth , Camera.main.pixelHeight);
+
+        string saveFile = GetPhotoPath("screenshot.png");
+
+        Texture2D texture= Capture.GetScreenshotAndSave(captureRect, saveFile);
+    }
+
+    private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
+    {
+        Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
+        Color[] rpixels = result.GetPixels(0);
+        float incX = ((float)1 / source.width) * ((float)source.width / targetWidth);
+        float incY = ((float)1 / source.height) * ((float)source.height / targetHeight);
+        for (int px = 0; px < rpixels.Length; px++)
+        {
+            rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
+        }
+        result.SetPixels(rpixels, 0);
+        result.Apply();
+        return result;
+    }
+}
---
+++ Assets/YHLib/Scripts/Utils/Capture.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace YH
+{
+	public class Capture
+	{
+		public static void SaveScreenshot(string file)
+		{
+			Application.CaptureScreenshot(file);
+		}
+
+		/// <summary>
+		/// 最好在一帧最后截图
+		/// </summary>
+		/// <param name="rect"></param>
+		/// <returns></returns>
+		public static Texture2D GetScreenshot(Rect rect)
+		{
+			Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+
+			//读取屏幕像素信息并存储为纹理数据。
+			screenShot.ReadPixels(rect, 0, 0);
+			screenShot.Apply();
+
+			return screenShot;
+		}
+
+		public static Texture2D GetScreenshotAndSave(Rect rect, string file)
+		{
+			Texture2D screenShot = GetScreenshot(rect);
+
+			// 然后将这些纹理数据，成一个png图片文件
+			byte[] bytes = screenShot.EncodeToPNG();
+
+			System.IO.File.WriteAllBytes(file, bytes);
+
+			return screenShot;
+		}
+	}
+}
