e11cca2c1c540bd992e9bfc2bf302fd288fb2f95
trarck <trarck@gmail.com>
2017-03-05 21:02:07 +0800

    add assets manager

---
+++ Assets/AssetsManager/Scripts/Manifest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace YH.AM
+{
+    [System.Serializable]
+    public class Manifest
+    {
+        /// <summary>
+        /// manifest包含的整个资源大小
+        /// </summary>
+        long m_TotalSize;
+
+        //具体内容
+        List<Asset> m_Assets;
+
+        public Manifest()
+        {
+            m_TotalSize = 0;
+            m_Assets = new List<Asset>();
+        }
+
+        public void Parse(string file)
+        {
+            string content="";
+            if(File.Exists(file))
+            {
+                content = File.ReadAllText(file);
+            }
+
+            if(!string.IsNullOrEmpty(content))
+            {
+
+            }
+        }
+
+        public void AddAsset(Asset asset)
+        {
+            m_Assets.Add(asset);
+            m_TotalSize += asset.size;
+        }
+
+        public Dictionary<string, object> ToDictionary()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["totalSize"] = m_TotalSize;
+
+            List<Dictionary<string, object>> assets = new List<Dictionary<string, object>>();
+            foreach(Asset asset in m_Assets)
+            {
+                assets.Add(asset.ToDictionary());
+            }
+            data["assets"] = assets;
+            return data;
+        }
+    }
+
+
+}
---
+++ Assets/Tests/Scripts/TestMd5.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+public class TestMd5 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+    string ByteToString(byte[] data)
+    {
+        StringBuilder sBuilder = new StringBuilder();
+        for (int i = 0; i < data.Length; ++i)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+        return sBuilder.ToString();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
