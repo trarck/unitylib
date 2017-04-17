51f21be096267f5d0ce8a19a21ae3ed77864dc2f
trarck <trarck@gmail.com>
2017-04-17 14:50:08 +0800

    把构建功能放在一个类里，这样做继承和其它操作。如果只是静态方法，不好扩展。

---
+++ Assets/YHLib/Editor/Tools/AutoBuild.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.IO;
+
+namespace YH
+{
+    public class AutoBuild
+    {     
+	
+        public void Build(BuildTarget target,Dictionary<string,string>options)
+        {
+            //check build to path
+            string outPath = options.ContainsKey("outpath")? options["outpath"]:null;
+            if (string.IsNullOrEmpty(outPath))
+            {
+                EditorUtility.DisplayDialog("build", "no out path", "ok");
+                return;
+            }
+
+			//parse resource
+			ParseResources(target, options);
+
+			//parse plugin.like sdk, lib...
+			ParsePlugins(target,options);
+
+			//set project settings
+			SetProjectSettings(target,options);      
+
+            //levels to build
+            string[] levels = null;
+            if (options.ContainsKey("levels"))
+            {
+                levels = options["levels"].Split(',');
+            }
+            else
+            {
+                levels = GetDefaultBuildScenes();
+            }
+
+            //build options
+            BuildOptions buildOpts = BuildOptions.None;
+            if (options.ContainsKey("buildOptionsDevelopment"))
+            {
+                buildOpts |= BuildOptions.Development;
+            }
+
+            if (options.ContainsKey("buildOptionsAllowDebugging"))
+            {
+                buildOpts |= BuildOptions.AllowDebugging;
+            }
+            
+			//remove exists file or directory
+			if(File.Exists(outPath))
+			{
+				File.Delete(outPath);
+			}else if(Directory.Exists(outPath))
+			{
+                //Directory.Delete(outPath, true);
+                FileSystemUtil.ForceDeleteDirectory(outPath);
+            }
+			
+            //call unity build function
+            BuildPipeline.BuildPlayer(levels, outPath, target, buildOpts);
+        }
+		
+		void SetProjectSettings(BuildTarget target,Dictionary<string,string>options)
+		{
+			//set normal 
+			SetCommonSettings(options);
+
+			//set special platform 
+			Type t = typeof(CmdAutoBuild);
+			Debug.Log("cmd:" + "Set" + UcFirst(target.ToString()) + "Settings");
+			MethodInfo method = t.GetMethod("Set" + UcFirst(target.ToString()) + "Settings", BindingFlags.NonPublic | BindingFlags.Static);
+			if (method != null)
+			{
+				object[] parameters = new object[1];
+				parameters[0] = options;
+				method.Invoke(null, parameters);
+			}
+		}
+
+		void SetCommonSettings(Dictionary<string,string> options)
+        {
+            //bundleIdentifier
+            if (options.ContainsKey("bundleIdentifier"))
+            {
+                PlayerSettings.bundleIdentifier = options["bundleIdentifier"];
+            }
+
+            //bundleVersion
+            if (options.ContainsKey("bundleVersion"))
+            {
+                PlayerSettings.bundleVersion = options["bundleVersion"];
+            }
+
+            //stripEngineCode
+            if (options.ContainsKey("stripEngineCode"))
+            {
+                if (options["stripEngineCode"].Equals("true", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    PlayerSettings.stripEngineCode = true;
+                }
+                else
+                {
+                    PlayerSettings.stripEngineCode = false;
+                }
+            }
+        }
+
+        void SetAndroidSettings(Dictionary<string, string> options)
+        {
+            //版本号
+            if (options.ContainsKey("bundleVersionCode"))
+            {
+                PlayerSettings.Android.bundleVersionCode = int.Parse(options["bundleVersionCode"]);
+            }
+			else
+			{
+				//自动加1
+				PlayerSettings.Android.bundleVersionCode = PlayerSettings.Android.bundleVersionCode +1;
+			}
+        }
+
+        void SetIOSSettings(Dictionary<string, string> options)
+        {
+            //版本号
+            if (options.ContainsKey("buildNumber"))
+            {
+                PlayerSettings.iOS.buildNumber = options["buildNumber"];
+            }
+			else
+			{
+				//自动加1
+				PlayerSettings.iOS.buildNumber = (int.Parse(PlayerSettings.iOS.buildNumber) + 1).ToString();
+			}
+        }
+		
+		void ParseResources(BuildTarget target,Dictionary<string,string>options)
+		{
+			//build special plateform
+			Type t = typeof(CmdAutoBuild);
+			MethodInfo method = t.GetMethod("Parse" + UcFirst(target.ToString()) + "Resource", BindingFlags.NonPublic | BindingFlags.Static);
+			if (method != null)
+			{
+				object[] parameters = new object[1];
+				parameters[0] = options;
+				method.Invoke(null, parameters);
+			}
+		}
+
+		void ParseAndroidResource(Dictionary<string, string> options)
+		{
+
+		}
+
+        void ParseIOSResource(Dictionary<string, string> options)
+        {
+
+        }
+
+        void ParsePlugins(BuildTarget target,Dictionary<string, string> options)
+		{
+            //special platform plugin
+			Type t = typeof(CmdAutoBuild);
+			MethodInfo method = t.GetMethod("Parse" + UcFirst(target.ToString()) + "Plugins", BindingFlags.NonPublic | BindingFlags.Static);
+			if (method != null)
+			{
+				object[] parameters = new object[1];
+				parameters[0] = options;
+				method.Invoke(null, parameters);
+			}
+		}
+		
+		void ParseAndroidPlugins(Dictionary<string,string> options)
+		{
+			
+		}
+
+        void ParseIOSPlugins(Dictionary<string, string> options)
+        {
+
+        }
+
+        static string[] GetDefaultBuildScenes()
+        {
+            List<string> names = new List<string>();
+            foreach (EditorBuildSettingsScene e in EditorBuildSettings.scenes)
+            {
+                if (e == null)
+                    continue;
+                if (e.enabled)
+                    names.Add(e.path);
+            }
+            return names.ToArray();
+        }
+
+        static string UcFirst(string str)
+        {
+            return str.Substring(0, 1).ToUpper() + str.Substring(1);
+        }
+    }
+}
--- Assets/YHLib/Editor/Tools/CmdAutoBuild.cs
+++ Assets/YHLib/Editor/Tools/CmdAutoBuild.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Reflection;
 using System;
+using System.IO;
 
 namespace YH
 {
@@ -29,177 +30,11 @@ namespace YH
                 EditorUtility.DisplayDialog("build", "no build target", "ok");
             }            
         }
-		
-        public static void Build(BuildTarget target,Dictionary<string,string>options)
+
+        public static void Build(BuildTarget target, Dictionary<string, string> options)
         {
-            string outPath = options.ContainsKey("outpath")? options["outpath"]:null;
-            if (string.IsNullOrEmpty(outPath))
-            {
-                EditorUtility.DisplayDialog("build", "no out path", "ok");
-                return;
-            }
-
-			//parse resource
-			ParseResources(target, options);
-
-			//parse plugin. sdk,lib...
-			ParsePlugins(target,options);
-
-			//set project settings
-			SetProjectSettings(target,options);      
-
-            //levels
-            string[] levels = null;
-            if (options.ContainsKey("levels"))
-            {
-                levels = options["levels"].Split(',');
-            }
-            else
-            {
-                levels = GetDefaultBuildScenes();
-            }
-
-            //build options
-            BuildOptions buildOpts = BuildOptions.None;
-            if (options.ContainsKey("buildOptionsDevelopment"))
-            {
-                buildOpts |= BuildOptions.Development;
-            }
-
-            if (options.ContainsKey("buildOptionsAllowDebugging"))
-            {
-                buildOpts |= BuildOptions.AllowDebugging;
-            }
-            
-			//remove exists file
-			if(File.Exists(outPath))
-			{
-				File.Delete(outPath);
-			}else if(Directory.Exists(outPath))
-			{
-				Directory.Delete(outPath, true);
-			}
-			
-            BuildPipeline.BuildPlayer(levels, outPath, target, buildOpts);
-        }
-		
-		static void SetProjectSettings(BuildTarget target,Dictionary<string,string>options)
-		{
-			//set normal 
-			SetCommonSettings(options);
-
-			//set special platform 
-			Type t = typeof(CmdAutoBuild);
-			Debug.Log("cmd:" + "Set" + UcFirst(target.ToString()) + "Settings");
-			MethodInfo method = t.GetMethod("Set" + UcFirst(target.ToString()) + "Settings", BindingFlags.NonPublic | BindingFlags.Static);
-			if (method != null)
-			{
-				object[] parameters = new object[1];
-				parameters[0] = options;
-				method.Invoke(null, parameters);
-			}
-		}
-
-		static void SetCommonSettings(Dictionary<string,string> options)
-        {
-            if (options.ContainsKey("bundleIdentifier"))
-            {
-                PlayerSettings.bundleIdentifier = options["bundleIdentifier"];
-            }
-
-            if (options.ContainsKey("bundleVersion"))
-            {
-                PlayerSettings.bundleVersion = options["bundleVersion"];
-            }
-        }
-
-        static void SetAndroidSettings(Dictionary<string, string> options)
-        {
-            if (options.ContainsKey("bundleVersionCode"))
-            {
-                PlayerSettings.Android.bundleVersionCode = int.Parse(options["bundleVersionCode"]);
-            }
-			else
-			{
-				//自动加1
-				PlayerSettings.Android.bundleVersionCode = PlayerSettings.Android.bundleVersionCode +1;
-			}
-        }
-
-        static void SetIOSSettings(Dictionary<string, string> options)
-        {
-            if (options.ContainsKey("buildNumber"))
-            {
-                PlayerSettings.iOS.buildNumber = options["buildNumber"];
-            }
-			else
-			{
-				//自动加1
-				PlayerSettings.iOS.buildNumber = (int.Parse(PlayerSettings.iOS.buildNumber) + 1).ToString();
-			}
-        }
-		
-		static void ParseResources(BuildTarget target,Dictionary<string,string>options)
-		{
-			//build special plateform
-			Type t = typeof(CmdAutoBuild);
-			MethodInfo method = t.GetMethod("Parse" + UcFirst(target.ToString()) + "Resource", BindingFlags.NonPublic | BindingFlags.Static);
-			if (method != null)
-			{
-				object[] parameters = new object[1];
-				parameters[0] = options;
-				method.Invoke(null, parameters);
-			}
-		}
-
-		static void BuildAndroidResource(Dictionary<string, string> options)
-		{
-
-		}
-	
-		static void ParsePlugins(BuildTarget target,Dictionary<string, string> options)
-		{
-			//libs
-
-			//sdks
-			Type t = typeof(CmdAutoBuild);
-			MethodInfo method = t.GetMethod("Parse" + UcFirst(target.ToString()) + "Plugins", BindingFlags.NonPublic | BindingFlags.Static);
-			if (method != null)
-			{
-				object[] parameters = new object[1];
-				parameters[0] = options;
-				method.Invoke(null, parameters);
-			}
-		}
-		
-		static void ParseAndroidPlugins(Dictionary<string,string> options)
-		{
-			string sdk = options.ContainsKey("sdk")? options["sdk"]:"Base";
-			string sdkPath=Application.dataPath+"../PackTools/"+sdk;
-
-			EditorHelper.DeleteFolder(AutoPack.AndroidPluginPath);
-			
-			EditorHelper.CopyDirectory(sdkPath, AutoPack.AndroidPluginPath);
-			
-			AssetDatabase.Refresh();
-		}
-	
-        static string[] GetDefaultBuildScenes()
-        {
-            List<string> names = new List<string>();
-            foreach (EditorBuildSettingsScene e in EditorBuildSettings.scenes)
-            {
-                if (e == null)
-                    continue;
-                if (e.enabled)
-                    names.Add(e.path);
-            }
-            return names.ToArray();
-        }
-
-        static string UcFirst(string str)
-        {
-            return str.Substring(0, 1).ToUpper() + str.Substring(1);
+            AutoBuild autoBuild = new AutoBuild();
+            autoBuild.Build(target, options);
         }
 
         [MenuItem("MyMenu/Test Cmd Build")]
@@ -209,7 +44,7 @@ namespace YH
             dict.Add("outpath", "d:\\temp\\a\\a.apk");
             dict.Add("bundleVersion", "1.0.6");
             dict.Add("bundleVersionCode", "6");
-            CmdAutoBuild._Build(BuildTarget.Android, dict);
+            CmdAutoBuild.Build(BuildTarget.Android, dict);
         }
     }
 }
