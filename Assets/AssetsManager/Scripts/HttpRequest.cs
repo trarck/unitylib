2f8ca6372ecd1d74ab20f1006c2248ef08ec7ae1
trarck <trarck@gmail.com>
2017-05-08 16:23:25 +0800

    加入http进度回调。

--- Assets/AssetsManager/Scripts/HttpRequest.cs
+++ Assets/AssetsManager/Scripts/HttpRequest.cs
@@ -4,34 +4,46 @@ using System.Collections.Generic;
 
 public class HttpRequest : MonoBehaviour {
 
-    public delegate void  RequestCallback(string err, WWW www);
+    public delegate void RequestCallback(string err, WWW www);
+    public delegate void ProgressCallback(float percent);
 
-	public void Get(string url,RequestCallback callback)
+    public float progressInterval = 0.1f;
+
+    public void Get(string url,RequestCallback callback, ProgressCallback progressCallback=null)
     {
-        StartCoroutine(Request(url, callback));
+        StartCoroutine(Request(url, callback, progressCallback));
     }
 
-    public void Post(string url, WWWForm form,RequestCallback callback)
+    public void Post(string url, WWWForm form,RequestCallback callback, ProgressCallback progressCallback = null)
     {
-        StartCoroutine(Request(url, form, callback));
+        StartCoroutine(Request(url, form, callback, progressCallback));
     }
 
-    public void Post(string url, byte[] postData, RequestCallback callback)
+    public void Post(string url, byte[] postData, RequestCallback callback, ProgressCallback progressCallback = null)
     {
-        StartCoroutine(Request(url, postData, callback));
+        StartCoroutine(Request(url, postData, callback, progressCallback));
     }
 
-    public void Post(string url, byte[] postData, Dictionary<string, string> headers, RequestCallback callback)
+    public void Post(string url, byte[] postData, Dictionary<string, string> headers, RequestCallback callback, ProgressCallback progressCallback = null)
     {
-        StartCoroutine(Request(url, postData, headers,callback));
+        StartCoroutine(Request(url, postData, headers,callback, progressCallback));
     }
 
     public IEnumerator Request(string url, RequestCallback callback)
     {
-        WWW www = new WWW(url); ;
+        WWW www = new WWW(url);
 
         yield return www;
 
+        callback(www.error, www);
+    }
+
+    public IEnumerator Request(string url, RequestCallback callback, ProgressCallback progressCallback)
+    {
+        WWW www = new WWW(url);
+        //StartCoroutine(HandleProgress(www, progressCallback));
+        StartCoroutine(HandleProgress(www, progressCallback));
+        yield return www;
         callback(www.error, www);
     }
 
@@ -44,6 +56,14 @@ public class HttpRequest : MonoBehaviour {
         callback(www.error, www);
     }
 
+    public IEnumerator Request(string url, WWWForm form, RequestCallback callback, ProgressCallback progressCallback)
+    {
+        WWW www = new WWW(url, form);
+        StartCoroutine(HandleProgress(www, progressCallback));
+        yield return www;
+        callback(www.error, www);
+    }
+
     public IEnumerator Request(string url, byte[] postData,RequestCallback callback)
     {
         WWW www = new WWW(url, postData);
@@ -53,7 +73,15 @@ public class HttpRequest : MonoBehaviour {
         callback(www.error, www);
     }
 
-    public IEnumerator Request(string url, byte[] postData, Dictionary<string, string> headers,RequestCallback callback)
+    public IEnumerator Request(string url, byte[] postData, RequestCallback callback, ProgressCallback progressCallback)
+    {
+        WWW www = new WWW(url, postData);
+        StartCoroutine(HandleProgress(www, progressCallback));
+        yield return www;
+        callback(www.error, www);
+    }
+
+    public IEnumerator Request(string url, byte[] postData, Dictionary<string, string> headers,RequestCallback callback, ProgressCallback progressCallback=null)
     {
         WWW www = null;
 
@@ -73,8 +101,23 @@ public class HttpRequest : MonoBehaviour {
             }           
         }
 
+        if(progressCallback!=null)
+        {
+            StartCoroutine(HandleProgress(www, progressCallback));
+        }
+
         yield return www;
 
         callback(www.error, www);
+    }
+    private IEnumerator HandleProgress(WWW www, ProgressCallback progressCallback)
+    {
+        while (!www.isDone)
+        {
+            progressCallback(www.progress);
+            yield return new WaitForSeconds(progressInterval); // WaitForEndOfFrame();
+        }
+        //这里不需要再调用百分白时候的回调,在这以前已经执行RequestCallback的回调.可以在RequestCallback里把显示进度设置成100%
+        //progressCallback(1.0f);
     }
 }
--- Assets/Tests/Scripts/TestHttp.cs
+++ Assets/Tests/Scripts/TestHttp.cs
@@ -9,9 +9,10 @@ public class TestHttp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        string patchPacktUrl = "http://patch.xuebaogames.com/monster/1.0.0_1.0.6.zip";
+        string patchPacktUrl = "http://patch.xuebaogames.com/monster/1.0.6.zip"; //"http://sw.bos.baidu.com/sw-search-sp/software/62fd1872480b4/iTunes_12.6.0.100_Setup.exe";// 
         m_HttpRequest.Get(patchPacktUrl, (err, www) =>
         {
+            Debug.Log("End " + Time.frameCount);
             if (err != null)
             {
                 Debug.Log(err);
@@ -21,11 +22,46 @@ public class TestHttp : MonoBehaviour {
                 byte[] bytes = www.bytes;
                 Debug.Log(bytes.Length);
             }
+        }, (percent) =>
+        {
+            Debug.LogFormat("state {0} {1}", percent, Time.frameCount);
         });
+        Debug.Log("test start");
+
+        //StartCoroutine(m_HttpRequest.Request(patchPacktUrl,
+        //    (err, www)=>{
+        //        Debug.Log("End "+err);
+        //    },
+        //    (p) =>
+        //    {
+        //        Debug.Log("p " + p);
+        //    }
+        //));
+        //StartCoroutine(DownloadFromUrl(patchPacktUrl));
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    private IEnumerator ShowProgress(WWW www)
+    {
+        Debug.Log("Show progress " + Time.frameCount);
+        while (!www.isDone)
+        {
+            Debug.LogFormat("Download {0:p1} {1}", www.progress,Time.frameCount);
+            yield return new WaitForEndOfFrame();//WaitForSeconds(0.1f);
+        }
+        Debug.Log("Done " + Time.frameCount);
+    }
+
+    private IEnumerator DownloadFromUrl(string strUrl)
+    {
+        Debug.Log("download start :"+Time.frameCount);
+        WWW www = new WWW(strUrl);
+        StartCoroutine(ShowProgress(www));
+        yield return www;
+        Debug.Log("test " + Time.frameCount);
+    }
 }
