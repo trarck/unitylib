76103f3aa6fcc86a38785235ec1ce224ccb4c8d6
trarck <trarck@gmail.com>
2017-08-01 11:29:18 +0800

    测试监听资源加载。

---
+++ Assets/Tests/Scripts/TestAssetsMonitor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using YH;
+
+public class TestAssetsMonitor : MonoBehaviour {
+
+    float m_Duration = 4.0f;
+
+    float m_Elapsed = 0;
+    // Use this for initialization
+    void Start () {
+        AssetsMonitor am=AssetsMonitor.Instance;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_Elapsed += Time.fixedDeltaTime;
+        if (m_Elapsed >= m_Duration)
+        {
+            m_Elapsed -= m_Duration;
+
+            if (SceneManager.GetActiveScene().name == "TestAssestUnload1")
+            {
+                SceneManager.LoadScene("TestAssestUnload2");
+            }
+            else
+            {
+                SceneManager.LoadScene("TestAssestUnload1");
+            }
+        }
+    }
+}
