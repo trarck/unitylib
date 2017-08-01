85c7de48c72fa589838391a95121cc17eeb13333
trarck <trarck@gmail.com>
2017-08-01 11:28:05 +0800

    加入目录处理。

--- Assets/YHLib/Scripts/Utils/FileSystemUtil.cs
+++ Assets/YHLib/Scripts/Utils/FileSystemUtil.cs
@@ -30,5 +30,44 @@ namespace YH
             }
             root.Delete(true);
         }
+
+        public static void ForceClearDirectory(string path)
+        {
+            DirectoryInfo root;
+            Stack<DirectoryInfo> fols;
+            DirectoryInfo fol;
+            fols = new Stack<DirectoryInfo>();
+            root = new DirectoryInfo(path);
+            fols.Push(root);
+            while (fols.Count > 0)
+            {
+                fol = fols.Peek();
+                fol.Attributes = fol.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+
+                foreach (FileInfo f in fol.GetFiles())
+                {
+                    f.Attributes = f.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                    f.Delete();
+                }
+
+                DirectoryInfo[] subs = fol.GetDirectories();
+                if (subs.Length > 0)
+                {
+                    foreach (DirectoryInfo d in fol.GetDirectories())
+                    {
+                        fols.Push(d);
+                    }
+                }
+                else
+                {
+                    if (fol != root)
+                    {
+                        fol.Delete(true);
+                    }
+
+                    fols.Pop();
+                }
+            }
+        }
     }
 }
