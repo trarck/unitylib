02be4d4aaf07c5e17c86ea414bf823a8c612f475
duanhouhai <duanhouhai@shinezone.com>
2017-12-15 15:43:58 +0800

    加入删除目录下的文件。

--- Assets/YHLib/Scripts/Utils/FileSystemUtil.cs
+++ Assets/YHLib/Scripts/Utils/FileSystemUtil.cs
@@ -69,5 +69,44 @@ namespace YH
                 }
             }
         }
+		
+		public static void RemoveDirectoryFiles(string path,string pattern)
+        {
+            DirectoryInfo root;
+            Stack<DirectoryInfo> fols;
+            DirectoryInfo fol;
+            fols = new Stack<DirectoryInfo>();
+            root = new DirectoryInfo(path);
+            fols.Push(root);
+            System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            while (fols.Count > 0)
+            {
+                fol = fols.Peek();
+                foreach (FileInfo f in fol.GetFiles())
+                {
+                    Debug.Log(f.Name + ":" + reg.IsMatch(f.Name));
+
+                    if (reg.IsMatch(f.Name))
+                    { 
+                        f.Attributes = f.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                        f.Delete();
+                    }
+                }
+
+                DirectoryInfo[] subs = fol.GetDirectories();
+                if (subs.Length > 0)
+                {
+                    foreach (DirectoryInfo d in fol.GetDirectories())
+                    {
+                        fols.Push(d);
+                    }
+                }
+                else
+                {
+                    fols.Pop();
+                }
+            }
+        }
     }
 }
