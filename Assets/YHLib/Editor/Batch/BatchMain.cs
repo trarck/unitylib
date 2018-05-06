bbafb85fd51dd307e6770cd12d187a19fb47f4b5
trarck <trarck@gmail.com>
2018-05-06 22:44:10 +0800

    添加批量处理资源。

---
+++ Assets/YHLib/Editor/Batch/BatchMain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace YH
+{
+
+    public class BatchMain : EditorWindow
+    {
+        [SerializeField]
+        FindTab m_FindTab;
+
+        [MenuItem("Window/AssetBatch", priority = 2050)]
+        static void ShowWindow()
+        {
+            var window = GetWindow<BatchMain>();
+            window.titleContent = new GUIContent("AssetBatch");
+            window.Show();
+        }
+    }
+
+}
---
+++ Assets/YHLib/Editor/Batch/FindTab.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace YH
+{
+    [System.Serializable]
+    public class FindTab
+    {
+        [SerializeField]
+        string m_SearchPath;
+
+        // Use this for initialization
+        public void Init(EditorWindow parent)
+        {
+
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+
+        }
+
+        public void OnGUI(Rect pos)
+        {
+            GUILayout.BeginHorizontal();
+            var newPath = EditorGUILayout.TextField("Search Path", m_SearchPath);
+            if ((newPath != m_SearchPath) &&
+                 (newPath != string.Empty))
+            {
+                m_SearchPath = newPath;
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
