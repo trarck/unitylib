5a9179466c6c12f70faa787dadb6cf086695ffd6
trarck <houhai@narvii.com>
2019-04-24 19:28:31 +0800

    加入场景自动保存。

---
+++ Assets/YHLib/Editor/Scenes/SceneSaver.cs
@@ -0,0 +1,210 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using Newtonsoft.Json;
+using UnityEditor.SceneManagement;
+using UnityEditorInternal;
+
+namespace YH
+{
+    public class SceneSaver
+    {
+        public string dataExt = ".json";
+
+        public string saveDir = null;
+
+        private Scene m_Scene;
+        public Scene scene
+        {
+            get
+            {
+                return m_Scene;
+            }
+            set
+            {
+                m_Scene = value;
+            }
+        }
+        
+        public string Save()
+        {
+            if (!m_Scene.IsValid())
+            {
+                return null;
+            }
+
+            Dictionary<string, List<float>> data = new Dictionary<string, List<float>>();
+
+            Queue<Transform> saveList = new Queue<Transform>();
+
+            foreach(GameObject rootObj in scene.GetRootGameObjects())
+            {
+                saveList.Enqueue(rootObj.transform);
+            }
+
+            while (saveList.Count > 0)
+            {
+                Transform current = saveList.Dequeue();
+                string fullPath = YH.HierarchyUtil.FullPath(current);
+                data[fullPath] = GetTransformData(current);
+
+                for(int i = 0, l = current.childCount; i < l; ++i)
+                {
+                    saveList.Enqueue(current.GetChild(i));
+                }
+            }
+
+            string sceneDataPath = GetSaveFullPath();
+            string content = JsonConvert.SerializeObject(data);
+            File.WriteAllText(sceneDataPath, content);
+            return sceneDataPath;
+        }
+
+        public void Restore(string dataFile)
+        {
+            if (string.IsNullOrEmpty(dataFile))
+            {
+                return;
+            }
+
+            if (!Path.IsPathRooted(dataFile))
+            {
+                dataFile = Path.Combine(saveDir, dataFile);
+            }
+
+            string content = File.ReadAllText(dataFile);
+            Dictionary<string, List<float>> data = JsonConvert.DeserializeObject<Dictionary<string, List<float>>>(content);
+
+            Scene scene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
+
+            Queue<Transform> saveList = new Queue<Transform>();
+
+            foreach (GameObject rootObj in scene.GetRootGameObjects())
+            {
+                saveList.Enqueue(rootObj.transform);
+            }
+
+            while (saveList.Count > 0)
+            {
+                Transform current = saveList.Dequeue();
+                string fullPath = YH.HierarchyUtil.FullPath(current);
+
+                if (data.ContainsKey(fullPath))
+                {
+                    SetTransformData(current, data[fullPath]);
+                }
+
+                for (int i = 0, l = current.childCount; i < l; ++i)
+                {
+                    saveList.Enqueue(current.GetChild(i));
+                }
+            }
+        }
+
+        public void Remove(string dataFile)
+        {
+            if (!Path.IsPathRooted(dataFile))
+            {
+                dataFile = Path.Combine(saveDir, dataFile);
+            }
+            File.Delete(dataFile);
+        }
+
+        public List<string> GetSavedFileNames()
+        {
+            List<string> data = new List<string>();
+
+            if (Directory.Exists(saveDir))
+            {
+                //get files
+                string[] files = Directory.GetFiles(saveDir);
+
+                foreach (string filePath in files)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    if (fileName.StartsWith(m_Scene.name))
+                    {
+                        data.Add(Path.GetFileName(filePath));
+                    }
+                }
+            }
+            return data;
+        }
+
+        List<float> GetTransformData(Transform t)
+        {
+            List<float> d = new List<float>();
+            //local position
+            d.Add(t.localPosition.x);
+            d.Add(t.localPosition.y);
+            d.Add(t.localPosition.z);
+
+            //local rotation
+            d.Add(t.localRotation.x);
+            d.Add(t.localRotation.y);
+            d.Add(t.localRotation.z);
+            d.Add(t.localRotation.w);
+
+            //local scale
+            d.Add(t.localScale.x);
+            d.Add(t.localScale.y);
+            d.Add(t.localScale.z);
+            return d;
+        }
+
+        void SetTransformData(Transform t,List<float> d)
+        {
+            //local position
+            Vector3 localPosition = new Vector3(d[0],d[1],d[2]);
+            t.localPosition=localPosition;
+
+            //local rotation
+            Quaternion localRotation = new Quaternion(d[3], d[4], d[5], d[6]);
+            t.localRotation = localRotation;
+
+            //local scale
+            Vector3 localScale = new Vector3(d[7], d[8], d[9]);
+            t.localScale = localScale;
+        }
+
+        string GetSaveFullPath()
+        {
+            int maxIndex = 0;
+
+            if (!Directory.Exists(saveDir))
+            {
+                Directory.CreateDirectory(saveDir);
+            }
+            else
+            {
+                //get files
+                string[] files = Directory.GetFiles(saveDir);
+                //get last index
+
+                foreach (string fileName in files)
+                {
+                    string fileNameNoExt = Path.GetFileNameWithoutExtension(fileName);
+
+                    if (fileNameNoExt.StartsWith(m_Scene.name))
+                    {
+
+                        int p = fileNameNoExt.LastIndexOf("_");
+                        int index = int.Parse(fileNameNoExt.Substring(p + 1));
+                        if (index > maxIndex)
+                        {
+                            maxIndex = index;
+                        }
+                    }
+                }
+
+                ++maxIndex;
+            }
+
+            string saveDataFile = m_Scene.name + "_" + maxIndex + dataExt;
+
+            return Path.Combine(saveDir, saveDataFile);
+        }
+    }
+}
---
+++ Assets/YHLib/Editor/Scenes/SceneSaverWindow.cs
@@ -0,0 +1,151 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using Newtonsoft.Json;
+using UnityEditor.SceneManagement;
+using UnityEditorInternal;
+
+namespace YH
+{
+    public class SceneSaverWindow : EditorWindow
+    {
+        List<string> m_SaveDatas = new List<string>();
+        ReorderableList m_List;
+        string m_SelectDataFile;
+        Vector2 m_ListScrollPosition = Vector2.zero;
+        bool m_AutoSave = false;
+        float m_Interval = 10*60;
+
+        SceneSaver m_SceneSaver;
+
+        double m_StartTime = 0;
+
+        [MenuItem("Avatar/SceneTransformHelper")]
+        public static void ShowMe()
+        {
+            //Show existing window instance. If one doesn't exist, make one.
+            EditorWindow.GetWindow<SceneSaverWindow>(false, "Scene Saver");
+        }
+
+        private void OnEnable()
+        {
+            m_SceneSaver = new SceneSaver();
+
+            UnityEditor.SceneManagement.EditorSceneManager.sceneOpened += OnSceneOpen;
+            EditorApplication.update += OnUpdate;
+
+            Scene scene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
+            m_SceneSaver.saveDir = Path.Combine(Application.temporaryCachePath, scene.name);
+            m_SceneSaver.scene = scene;
+
+            m_SaveDatas = m_SceneSaver.GetSavedFileNames();
+
+            m_List = new ReorderableList(m_SaveDatas, typeof(string));
+
+            m_List.displayAdd = false;
+
+            m_List.drawHeaderCallback = (Rect rect) => {
+                EditorGUI.LabelField(rect, "Save Datas");
+            };
+
+            //m_List.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => {
+                
+            //};
+
+            m_List.onSelectCallback = (ReorderableList l) => {
+                m_SelectDataFile = l.list[l.index] as string;
+            };
+
+            m_List.onCanRemoveCallback = (ReorderableList l) => {
+                return l.count > 0;
+            };
+
+            m_List.onRemoveCallback = (ReorderableList l) => {
+                Remove(l.index);
+            };
+
+            //m_List.onAddCallback = (ReorderableList l) => {
+            //    Debug.Log(l.index);
+            //};
+
+            m_StartTime = EditorApplication.timeSinceStartup;
+
+        }
+
+        private void OnDisable()
+        {
+            EditorApplication.update -= OnUpdate;
+            UnityEditor.SceneManagement.EditorSceneManager.sceneOpened -= OnSceneOpen;
+        }
+
+        private void OnSceneOpen(Scene scene, OpenSceneMode mode)
+        {
+            m_SceneSaver.saveDir = Path.Combine(Application.temporaryCachePath, scene.name);
+            m_SceneSaver.scene = scene;
+            m_SaveDatas = m_SceneSaver.GetSavedFileNames();
+            m_List.list = m_SaveDatas;
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            m_AutoSave = EditorGUILayout.ToggleLeft("AutoSave",m_AutoSave, GUILayout.MinWidth(60));
+            YH.YHEditorTools.PushLabelWidth(80);
+            m_Interval = EditorGUILayout.FloatField("Interval(s)：", m_Interval, GUILayout.MinWidth(30));
+            YH.YHEditorTools.PopLabelWidth();
+            EditorGUILayout.EndHorizontal();
+
+            m_ListScrollPosition=EditorGUILayout.BeginScrollView(m_ListScrollPosition);
+            m_List.DoLayoutList();
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Save"))
+            {
+                Save();
+            }
+
+            if (GUILayout.Button("Restore"))
+            {
+                Restore();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void OnUpdate()
+        {
+            if (m_AutoSave && m_Interval > 0)
+            {
+                if (EditorApplication.timeSinceStartup - m_StartTime > m_Interval)
+                {
+                    Save();
+                    m_StartTime = EditorApplication.timeSinceStartup;
+                }
+            }
+        }
+
+        public void Save()
+        {
+            string saveFilePath=m_SceneSaver.Save();
+            if (!string.IsNullOrEmpty(saveFilePath))
+            {
+                m_SaveDatas.Add(Path.GetFileName(saveFilePath));
+            }
+        }
+
+        public void Restore()
+        {
+            m_SceneSaver.Restore(m_SelectDataFile);
+        }
+
+        public void Remove(int i)
+        {
+            m_SceneSaver.Remove(m_SaveDatas[i]);
+            m_SaveDatas.RemoveAt(i);
+        }
+    }
+}
+
