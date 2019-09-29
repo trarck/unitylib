d25c927b74e9e763f6c6b3e92ccb90d30b110bd1
trarck <houhai@narvii.com>
2019-09-29 11:03:55 +0800

    加入异步的处理库。

---
+++ Assets/YHLib/Scripts/Async/ActionGroup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YH.Async
+{
+    public class ActionGroup
+    {
+        public Action completeHandle;
+
+        int m_ActionCount=0;
+        int m_FinishedCount = 0;
+        bool m_Joined = false;
+
+        public ActionGroup()
+        {
+            m_ActionCount = 0;
+        }
+
+        public ActionGroup(int total)
+        {
+            m_ActionCount = total;
+        }
+
+        public void DoActionFinish()
+        {
+            ++m_FinishedCount;
+            //check finished
+            CheckFinished();
+        }
+
+        public void Increase()
+        {
+            ++m_ActionCount;
+        }
+
+        public void Increase(int count)
+        {
+            m_ActionCount+=count;
+        }
+
+        public void Decrease()
+        {
+            --m_ActionCount;
+        }
+
+        public void Decrease(int count)
+        {
+            m_ActionCount-=count;
+        }
+
+        public void Join()
+        {
+            m_Joined = true;
+            //check finished
+            CheckFinished();
+        }
+
+        void CheckFinished()
+        {
+            if (m_FinishedCount >= m_ActionCount && m_Joined)
+            {
+                if (completeHandle != null)
+                {
+                    completeHandle();
+                }
+            }
+        }
+
+        public void Clean()
+        {
+            m_ActionCount = 0;
+            m_FinishedCount = 0;
+            m_Joined = false;
+            completeHandle = null;
+        }
+    }
+}
---
+++ Assets/YHLib/Scripts/Async/Task.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH.Async
+{
+    public class Task
+    {
+        public int id;
+        //if method is static target is null
+        object m_Target;
+        MethodInfo m_Method;
+        TaskPool m_WorkPool;
+        object[] m_Args;
+
+        IEnumerator m_Enumerator;
+
+        public Task(int id, TaskPool workPool, object target, MethodInfo method, params object[] args)
+        {
+            this.id = id;
+            m_WorkPool = workPool;
+
+            m_Target = target;
+            m_Method = method;
+            SetArgs(args);
+        }
+
+        public Task(int id, TaskPool workPool, IEnumerator enumerator)
+        {
+            this.id = id;
+            m_WorkPool = workPool;
+
+        }
+
+        public void Run()
+        {
+            if (m_Method!=null)
+            {
+                IEnumerator ret = m_Method.Invoke(m_Target, m_Args) as IEnumerator;
+                if (ret != null && m_WorkPool!=null)
+                {
+                    m_WorkPool.StartCoroutine(ret);
+                }
+            }
+        }
+
+        public void Done()
+        {
+            m_WorkPool.FinishTask(this);
+        }
+
+        public void Error()
+        {
+            m_WorkPool.ErrorTask(this);
+        }
+
+        void SetArgs(object[] args)
+        {
+            m_Args = new object[args.Length + 1];
+
+            m_Args[0] = this;
+            for (int i = 0; i < args.Length; ++i)
+            {
+                m_Args[i + 1] = args[i];
+            }
+        }
+    }
+}
