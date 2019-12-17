6eda230fec6851b8def937e6f0152175e6640965
trarck <houhai@narvii.com>
2019-12-17 10:39:02 +0800

    conditional log

---
+++ Assets/YHLib/Scripts/Log/YHDebug.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace YH.Log
+{
+    public class YHDebug
+    {
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void Log(object message)
+        {
+            Debug.Log(message);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogFormat(string format, params object[] args)
+        {
+            Debug.LogFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogWarning(object message)
+        {
+            Debug.LogWarning(message);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogWarningFormat(string format, params object[] args)
+        {
+            Debug.LogWarningFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogError(object message)
+        {
+            Debug.LogError(message);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogErrorFormat(string format, params object[] args)
+        {
+            Debug.LogErrorFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogException(System.Exception exception, Object context)
+        {
+            Debug.LogException(exception, context);
+        }
+
+        [System.Diagnostics.Conditional("LOG_ENABLE")]
+        public static void LogException(System.Exception exception)
+        {
+            Debug.LogException(exception);
+        }
+    }
+}
