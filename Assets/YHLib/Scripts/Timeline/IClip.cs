b98b84a09b9c03c68a2db39d672ac3afb3b21916
trarck <trarck@gmail.com>
2020-08-15 10:22:47 +0800

    temp

---
+++ Assets/YHLib/Scripts/Timeline/IClip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH.Timeline
+{
+    public  interface IClip
+    {
+        double start { get; set; }
+        double end { get; set; }
+        double duration { get; set; }
+    }
+}
