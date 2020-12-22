3c58e605879307b20e12914b91e16d4030de7b70
duanhouhai <houhai.duan@mihoyo.com>
2020-12-22 15:21:58 +0800

    big tree

---
+++ Assets/Tests/Editor/UI/TestBigTreeView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+public class TestBigTreeView : EditorWindow
+{
+	TreeViewState _state;
+	BigTreeView _bigTreeView;
+	Vector2 _scrollPosition;
+
+	// Add menu item named "My Window" to the Window menu
+	[MenuItem("MyMenu/TestBigTreeView")]
+	public static void ShowWindow()
+	{
+		//Show existing window instance. If one doesn't exist, make one.
+		EditorWindow.GetWindow(typeof(TestBigTreeView));
+	}
+
+	void OnGUI()
+	{
+		if (_bigTreeView == null)
+		{
+			_state = new TreeViewState();
+
+			_bigTreeView = new BigTreeView(_state, null);
+			_bigTreeView.num = 100000;
+			_bigTreeView.Reload();
+		}
+
+
+
+		Rect r = new Rect(20,20,300,150);
+		_bigTreeView.OnGUI(r);
+
+		int a = 0;
+		int b = 0;
+		_bigTreeView.GetTest(out a, out b);
+		Debug.LogFormat("{0},{1}", a, b);
+ 	}
+}
---
+++ Assets/YHLib/Editor/BigTreeView.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+public class BigTreeView : TreeView
+{
+	public int num = 1;
+
+	public BigTreeView(TreeViewState state, MultiColumnHeader multiColumnHeader) :base(state,multiColumnHeader)
+	{
+		useScrollView = true;
+	}
+
+	protected override TreeViewItem BuildRoot()
+	{
+		TreeViewItem root = new TreeViewItem(1,-1, "tt");
+
+		TreeViewItem item = null;
+
+		for (int i = 0; i < num; ++i)
+		{
+			item = new TreeViewItem(i + 1, 0, i.ToString());
+			root.AddChild(item);
+		}
+
+		return root;
+	}
+
+	//protected override IList<TreeViewItem> BuildRows(TreeViewItem root)
+	//{
+
+	//	List<TreeViewItem> rows = new List<TreeViewItem>();
+	//	TreeViewItem item = null;
+
+	//	for (int i = 0; i < num; ++i)
+	//	{
+	//		item = new TreeViewItem(i+1, 0, i.ToString());
+	//		rows.Add(item);
+	//	}
+
+
+	//	//SetupParentsAndChildrenFromDepths(root, rows);
+
+	//	return rows;
+	//}
+
+	public void GetTest(out int a, out int b)
+	{
+		GetFirstAndLastVisibleRows(out a, out b);
+	}
+}
