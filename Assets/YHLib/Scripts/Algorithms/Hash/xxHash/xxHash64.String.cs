23da496808f45a80a52a342bf48346261504403f
trarck <trarck@gmail.com>
2023-12-30 14:04:48 +0800

    计算字符串hash加入线程级buffer缓存。不过缓存大小有限制。

--- Assets/YHLib/Scripts/Algorithms/Hash/xxHash/xxHash64.String.cs
+++ Assets/YHLib/Scripts/Algorithms/Hash/xxHash/xxHash64.String.cs
@@ -1,20 +1,34 @@
-﻿using System.Diagnostics;
+﻿using System.Threading;
 using System.Text;
 
 namespace YH.Hash.xxHash
 {
     public static partial class xxHash64
     {
+        private const int _StringBufferSize = 1024;
+        private static ThreadLocal<byte[]> _StringBuffer=new ThreadLocal<byte[]>();
         public static ulong ComputeHash(string s)
         {
             return ComputeHash(s, Encoding.UTF8, 0);
         }
 
-        public static ulong ComputeHash(string s , Encoding encoding, ulong seed = 0)
+        public static ulong ComputeHash(string s, Encoding encoding, ulong seed = 0)
         {
+            if (!_StringBuffer.IsValueCreated)
+            {
+                _StringBuffer.Value = new byte[_StringBufferSize];
+            }
+            byte[] buff = _StringBuffer.Value;
+            int byteLenth = encoding.GetBytes(s, 0, s.Length, buff, 0);
+            return ComputeHash(buff, byteLenth, seed);
+        }
 
-            byte[] buff = encoding.GetBytes(s);
-            return ComputeHash(buff, buff.Length,seed);
+        public unsafe static ulong ComputeHashStr(string s)
+        {
+            fixed (char* ps = s)
+            {
+                return ComputeHash((byte*)ps, s.Length * 2, 0);
+            }
         }
     }
 }
